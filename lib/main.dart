7bf2f5f4551b4b83cefc94be3bf3d9dac2b65d86
Darek-Mac <darek.baranczuk@gmail.com>
2019-05-19 21:14:10 +0200

    Changed icon, theme color, ui fixes

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,25 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   
+  Map<int, Color> color = {
+    50: Color.fromRGBO(199, 92, 92, .1),
+    100: Color.fromRGBO(199, 92, 92, .2),
+    200: Color.fromRGBO(199, 92, 92, .3),
+    300: Color.fromRGBO(199, 92, 92, .4),
+    400: Color.fromRGBO(199, 92, 92, .5),
+    500: Color.fromRGBO(199, 92, 92, .6),
+    600: Color.fromRGBO(199, 92, 92, .7),
+    700: Color.fromRGBO(199, 92, 92, .8),
+    800: Color.fromRGBO(199, 92, 92, .9),
+    900: Color.fromRGBO(199, 92, 92, 1),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Reader',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFFC75C5C, color),
       ),
       home: ReaderPage(),
     );
--- lib/tags/tags.dart
+++ lib/tags/tags.dart
@@ -57,18 +57,21 @@ class _TagsState extends State<Tags> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: widget._tagStorage.fetchTags(),
-        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-          return CommonWidgets.handleFuture(snapshot, (json) {
-            return ListView.builder(
-              itemCount: (json as List<String>).length,
-              itemBuilder: (BuildContext context, index) =>
-                  _tagItem((json as List<String>)[index]),
-            );
-          });
-        },
-      ),
+      body: Padding(
+          padding: EdgeInsets.only(top: 8),
+          child: FutureBuilder(
+            future: widget._tagStorage.fetchTags(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+              return CommonWidgets.handleFuture(snapshot, (json) {
+                return ListView.builder(
+                  itemCount: (json as List<String>).length,
+                  itemBuilder: (BuildContext context, index) =>
+                      _tagItem((json as List<String>)[index]),
+                );
+              });
+            },
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _addTagDialog(context),
         tooltip: 'Add tag',
@@ -78,6 +81,13 @@ class _TagsState extends State<Tags> {
   }
 
   Widget _tagItem(String tagName) {
-    return Tag(tagName);
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Tag(tagName),
+          Padding(
+              padding: EdgeInsets.only(left: 16, right: 16), child: Divider())
+        ]);
   }
 }
